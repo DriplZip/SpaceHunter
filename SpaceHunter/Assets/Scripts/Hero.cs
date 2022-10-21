6e3d5199541fbeaff20af3d86c4debc476acc199
Artem <artearteshev@gmail.com>
2022-10-22 02:14:51 +0300

    feat: add hero movment and shield animation

---
+++ SpaceHunter/Assets/Scripts/Hero.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private static Hero _s;
+
+    public static Hero S => _s;
+    
+    [Header("Set in Inspector")]
+    [SerializeField] private float speed = 30;
+    [SerializeField] private float rollMulti = -45;
+    [SerializeField] private float rotationMulti = 30;
+
+    private int _shieldLvl = 1;
+
+    public int SheildLvl => _shieldLvl;
+
+    private void Awake()
+    {
+        if (_s == null)
+            _s = this;
+        else
+            Debug.LogError("Hero.Awake() - Attempt to create not a single hero");
+    }
+
+    private void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 position = transform.position;
+        position.x += xAxis * speed * Time.deltaTime;
+        position.y += yAxis * speed * Time.deltaTime;
+        transform.position = position;
+
+        transform.rotation = Quaternion.Euler(yAxis * rotationMulti, xAxis * rotationMulti, 0);
+    }
+}
