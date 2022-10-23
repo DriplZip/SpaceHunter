3d59693c02710a606cb5125f83f13c82e176a729
Artem <artearteshev@gmail.com>
2022-10-24 01:05:19 +0300

    feat: add Enemy_2

--- SpaceHunter/Assets/Scripts/Enemies/Enemy_2.cs
+++ SpaceHunter/Assets/Scripts/Enemies/Enemy_2.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class Enemy_2 : MonoBehaviour
+public class Enemy_2 : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Set in Inspector: Enemy_2")] 
+    [SerializeField] private float sinEccentricity = 0.6f;
+    [SerializeField] private float lifeTime = 10;
+
+    [Header("Set Dynamically: Enemy_2")] 
+    private Vector3 p0;
+    private Vector3 p1;
+    private float birthTime;
+
+    private void Start()
     {
+        p0 = Vector3.zero;
+        p0.x = -bordersCheck.CamWight - bordersCheck.RepulsionRadius;
+        p0.y = Random.Range(-bordersCheck.CamHeight, bordersCheck.CamHeight);
         
+        p1 = Vector3.zero;
+        p1.x = bordersCheck.CamWight + bordersCheck.RepulsionRadius;
+        p1.y = Random.Range(-bordersCheck.CamHeight, bordersCheck.CamHeight);
+
+        if (Random.value > 0.5f)
+        {
+            p0.x *= -1;
+            p1.x *= -1;
+        }
+
+        birthTime = Time.time;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Move()
     {
-        
+        float uBeziers = (Time.time - birthTime) / lifeTime;
+
+        if (uBeziers > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        uBeziers = uBeziers + sinEccentricity * (Mathf.Sin(uBeziers * Mathf.PI * 2));
+
+        Position = (1 - uBeziers) * p0 + uBeziers * p1;
     }
 }
