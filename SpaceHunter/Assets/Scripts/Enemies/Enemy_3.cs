a844adfafa9463826898268995941af63d291e69
Artem <artearteshev@gmail.com>
2022-10-25 04:58:27 +0300

    feat: add Enemy_3 (uBeziers bug)

--- SpaceHunter/Assets/Scripts/Enemies/Enemy_3.cs
+++ SpaceHunter/Assets/Scripts/Enemies/Enemy_3.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class Enemy_3 : MonoBehaviour
+public class Enemy_3 : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Set in Inspector: Enemy_3")]
+    [SerializeField] private float lifeTime = 5;
+
+    [Header("Set Dynamically: Enemy_3")] 
+    private Vector3[] points;
+    private float birthTime;
+
+    private void Start()
     {
+        points = new Vector3[3];
+        points[0] = Position;
+
+        float xMin = -bordersCheck.CamWight + bordersCheck.RepulsionRadius;
+        float xMax = bordersCheck.CamWight - bordersCheck.RepulsionRadius;
+
+        Vector3 point = Vector3.zero;
+        point.x = Random.Range(xMin, xMax);
+        point.y = -bordersCheck.CamHeight * Random.Range(2f, 2.5f);
         
+        points[1] = point;
+        
+        point = Vector3.zero;
+        point.y = Position.y;
+        point.x = Random.Range(xMin, xMax);
+
+        points[2] = point;
+
+        birthTime = Time.time;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Move()
     {
+        float uBeziers = (Time.time - birthTime) / lifeTime;
+
+        if (uBeziers > 1)
+        {
+            Destroy(gameObject);
+            
+            return;
+        }
+
+        Vector3 p01, p12;
+        uBeziers -= 0.2f * Mathf.Sin(uBeziers * Mathf.PI * 2); // Beziers smoothing
         
+        p01 = (1 - uBeziers) * points[0] + uBeziers * points[1];
+        p12 = (1 - uBeziers) * points[1] + uBeziers * points[2];
+
+        Position = (1 - uBeziers) * p01 + uBeziers * p12;
     }
 }
