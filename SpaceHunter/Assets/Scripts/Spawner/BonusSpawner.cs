4f592bac42472174bfec196f8a7f5a319b553fb8
Artem <artearteshev@gmail.com>
2022-10-28 02:53:25 +0300

    feat: add BonusSpawner

--- SpaceHunter/Assets/Scripts/Spawner/BonusSpawner.cs
+++ SpaceHunter/Assets/Scripts/Spawner/BonusSpawner.cs
@@ -5,12 +5,8 @@ using UnityEngine;
 public class BonusSpawner : MonoBehaviour
 {
     private static BonusSpawner s;
+    private static Dictionary<WeaponType, WeaponDefinition> WEAPON_DICT;
 
-    [Header("Set in Inspector")] [SerializeField]
-    private GameObject[] enemiesPrefab;
-
-    [SerializeField] private float enemySpawnPerSecond = 0.5f;
-    [SerializeField] private float enemyDefaultPadding = 1.5f;
     [SerializeField] private WeaponDefinition[] weaponDefinitions;
 
     private BordersCheck bordersCheck;
@@ -21,27 +17,11 @@ public class BonusSpawner : MonoBehaviour
 
         bordersCheck = GetComponent<BordersCheck>();
 
-        Invoke(nameof(SpawnEnemy), 1f/enemySpawnPerSecond);
-    }
+        WEAPON_DICT = new Dictionary<WeaponType, WeaponDefinition>();
 
-    private void SpawnEnemy()
-    {
-        int enemyPrefabIdx = Random.Range(0, enemiesPrefab.Length);
-        GameObject enemy = Instantiate(enemiesPrefab[enemyPrefabIdx]);
-        BordersCheck enemyBordersCheck = enemy.GetComponent<BordersCheck>();
-
-        float enemyPadding = enemyDefaultPadding;
-        if (enemyBordersCheck != null) enemyPadding = Mathf.Abs(enemyBordersCheck.RepulsionRadius);
-
-        Vector3 position = Vector3.zero;
-        float xMin = -bordersCheck.CamWight + enemyPadding;
-        float xMax = bordersCheck.CamWight - enemyPadding;
-
-        position.x = Random.Range(xMin, xMax);
-        position.y = bordersCheck.CamHeight + enemyPadding;
-
-        enemy.transform.position = position;
-        
-        Invoke(nameof(SpawnEnemy), 1f/enemySpawnPerSecond);
+        foreach (WeaponDefinition definition in weaponDefinitions)
+        {
+            WEAPON_DICT[definition.type] = definition;
+        }
     }
 }
